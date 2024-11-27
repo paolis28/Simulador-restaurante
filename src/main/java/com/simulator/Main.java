3346a16df40236ee8fad547c2461fec1b910ac06
JulianVic <josejulianvicenteperez@gmail.com>
2024-11-27 00:51:27 -0600

    Agregando distribución Poisson

--- src/main/java/com/simulator/Main.java
+++ src/main/java/com/simulator/Main.java
@@ -5,29 +5,37 @@ import java.util.List;
 
 import com.simulator.controllers.RestauranteMonitor;
 import com.simulator.models.Comensal;
+import com.simulator.utils.PoissonDistribution;
 
 
 public class Main {
     private static final int TOTAL_COMENSALES = 100;
+    private static final double LAMBDA = 2.0; // Tasa media de llegadas por minuto
 
     public static void main(String[] args) {
         RestauranteMonitor restaurante = new RestauranteMonitor();
         List<Thread> hilosComensales = new ArrayList<>();
+        PoissonDistribution poissonDist = new PoissonDistribution(LAMBDA);
 
         // Crear 100 comensales
         for (int i = 0; i < TOTAL_COMENSALES; i++) {
             Comensal comensal = new Comensal(i);
             Thread hiloComensal = new Thread(() -> {
                 try {
-                    boolean pudoEntrar = restaurante.intentarEntrar(comensal);
-                    if (pudoEntrar) {
-                        System.out.println("Comensal " + comensal.getId() + " entró al restaurante");
-                        // Aquí iría la lógica de estar en el restaurante
-                        Thread.sleep((long) (Math.random() * 5000)); // Simular tiempo en el restaurante
-                        restaurante.salirRestaurante();
-                        System.out.println("Comensal " + comensal.getId() + " salió del restaurante");
-                    } else {
-                        System.out.println("Comensal " + comensal.getId() + " no pudo entrar, restaurante lleno");
+                    Thread.sleep(poissonDist.getNextArrivalTime());
+                    boolean pudoEntrar = false;
+                    // Seguir intentando hasta lograr entrar
+                    while (!pudoEntrar) {
+                        pudoEntrar = restaurante.intentarEntrar(comensal);
+                        if (pudoEntrar) {
+                            System.out.println("Comensal " + comensal.getId() + " ha entró al restaurante");
+                            Thread.sleep((long) (Math.random() * 5000));
+                            restaurante.salirRestaurante();
+                            System.out.println("Comensal " + comensal.getId() + " ha salió del restaurante");
+                        } else {
+                            System.out.println("Comensal " + comensal.getId() + " ha de esperar, restaurante lleno");
+                            Thread.sleep(1000); // Esperar un momento antes de volver a intentar
+                        }
                     }
                 } catch (InterruptedException e) {
                     e.printStackTrace();
